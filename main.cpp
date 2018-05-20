15e427c28106f7f221d8afecf42cce7457f4b618
Michael Perna <michael.anthony.perna@gmail.com>
2018-05-20 15:32:13 -0700

    MemoryManager class implemented

--- main.cpp
+++ main.cpp
@@ -8,191 +8,31 @@
 
 #include "process.h"
 #include "memory.h"
-
-const int MAX_TIME = 100000;
-
-long last_announcement = -1;
-
-std::vector<Process> pl;
-FrameList fl;
-std::vector<Process> pq;
-
-void print_proc_queue(std::vector<Process> pq) {
-	std::cout << "\tInput queue: [";
-
-	for (auto el : pq) {
-   std::cout << el.get_pid();
-	}
-	std::cout <<  "]\n";
-}
-
-std::string get_announcement_prefix(long current_time)  {
-	std::string result = "\t";
-
-  if (last_announcement != current_time) 
-		result = "t = " + std::to_string(current_time) + ", ";
-
-	last_announcement = current_time;
-
-	return result;
-}
-
-void enqueue_newly_arrived(long current_time) {
-  for (auto &proc : pl) {
-	  if (proc.get_arrival() == current_time) {
-			std::cout << get_announcement_prefix(current_time) <<
-			  "Process " << proc.get_pid() << " arrives" << std::endl;
-		      
-			pq.push_back(proc);
-      print_proc_queue(pq);
-
-      fl.print();
-		}
-	}
-}
-
-void terminate_completed_process(long current_time) {
-	// dequeue any procs that need it
-	for (int i = 0; i < pl.size(); ++i) {
-
-	  if (pl[i].active()) {
-			int time_elapsed = current_time - pl[i].get_load_time();
-	    if (time_elapsed >= pl[i].get_life()) {
-			  std::cout << get_announcement_prefix(current_time)
-          << "process " << pl[i].get_pid() << " completes\n";
-
-			  pl[i].end(current_time);
-			  fl.free_by_pid(pl[i].get_pid());
-			  fl.print();
-		  }
-	  }
-  }
-}
-
-void update_pl(int pid, int current_time) {
-  for (auto &el : pl) 
-		if (el.get_pid() == pid) 
-		  el.load_to_mem(current_time);
-}
-
-void assign_available_mem(long current_time) {
-	for (int i = 0; i < pq.size(); ++i) {
-		Process proc = pq[i];
-
-		if (fl.process_fits(proc)) {
-			std::cout << get_announcement_prefix(current_time) 
-				<< "MM moves Process " << proc.get_pid() << " to memory\n";
-			
-		  fl.fit_process(proc);
-      
-			update_pl(proc.get_pid(), current_time);
-		
-		  pq.erase(pq.begin() + i);
-		  print_proc_queue(pq);
-			assign_available_mem(current_time);
-		  fl.print();		
-	  }
-	}
-}
-
-void print_turnaround_time()  {
-	float total = 0;
-
-	for (auto proc : pl) 
-		total += proc.get_time_done() - proc.get_arrival();
-	
-  float avg = total / pl.size();
-	std::cout << "Average Turnaround Time: " << avg << std::endl;
-}
-
-void main_loop() {
-	long current_time = 0;
-
-	do { 
-		enqueue_newly_arrived(current_time);
-    terminate_completed_process(current_time);
-		assign_available_mem(current_time);
-
-		++current_time;
-
-		if (current_time > MAX_TIME) {
-			std::cout << "DEADLOCK: max time reached\n";
-			break;
-		}
-  bool test = fl.empty();
-	bool test_pq = pq.empty();
-
-	} while (!(pq.empty() && fl.empty()));
-
-	print_turnaround_time();
-}
+#include "memorymanager.h"
 
 int main() {
-
-	int memSize = 0;
-	int pgSize = 0;
-	
-	//User input
-	std::string fileInput;
+	std::string file_name;
+	int mem_size, page_size;
 	
 	std::cout << "Memory size: ";
-	std::cin >> memSize;
+	std::cin >> mem_size;
 	std::cout << "Page size: ";
-	std::cin >> pgSize;
-	std::ifstream ifs;
+	std::cin >> page_size;
 
-	while (!(memSize % pgSize == 0)) {
+	while (!(mem_size % page_size == 0)) {
 		std::cout << "Error: Memory size must be a multiple of page size." << std::endl;
 		std::cout << "Please re-enter memory size and page size." << std::endl;
 		std::cout << "Memory size: ";
-		std::cin >> memSize;
+		std::cin >> mem_size;
 		std::cout << "Page size: ";
-		std::cin >> pgSize;
+		std::cin >> page_size;
 	}
 
 	std::cout << "Input file: ";
-	std::cin >> fileInput;
-	ifs.open(fileInput);
+	std::cin >> file_name;
 
-	while (!ifs.is_open()) {
-		std::cout << "Error: File could not be opened." << std::endl;
-		std::cout << "Please input the correct filename." << std::endl;
-		std::cout << "Input file: ";
-		std::cin >> fileInput;
-		ifs.open(fileInput);
-	}
-
-	//Process file input
-	long arrival, life_time;
-	int pid, tmp, num_space;
-	int counter = 0;
-	int total_space = 0;
-	int num_procs = 0;
-  
-	ifs >> num_procs;
-	pl = std::vector<Process>(num_procs);
-
-	while (!ifs.eof() && counter < num_procs) {
-    
-		// store temp values for processes
-		ifs >> pid >> arrival >> life_time >> num_space;
-			
-		total_space = 0;
-		for (int i = 0; i < num_space; ++i) {
-			ifs >> tmp;
-			total_space += tmp;
-		}
-
-		Process proc(pid, arrival, life_time, total_space);
-		pl[counter++] = proc;
-	}
-
-	ifs.close();
-
-	int x = memSize / pgSize;
-	fl = FrameList(x, pgSize);
-
-	main_loop();
+	MemoryManager mm(mem_size, page_size, file_name);
+	mm.simulate();
 
 	return 0;
 }
---
+++ memorymanager.h
@@ -0,0 +1,175 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <vector>
+
+#include "memory.h"
+#include "process.h"
+
+
+class MemoryManager {
+private:
+  const int MAX_TIME = 100000;
+  long current_time_;
+  long last_announcement_; // -1
+
+  std::vector<Process> pl_;
+  FrameList fl_;
+  std::vector<Process> pq_;
+
+  void update_pl(int pid);
+  void assign_process_list(std::string &filename);
+  void print_turnaround_time();
+  void enqueue_arrivals();
+  void print_process_queue() const;
+  void assign_available_memory();
+  void terminate_completed_processes();
+	std::string get_prefix();
+public:
+  MemoryManager(int mem, int page_size, std::string &filename);
+  void simulate();
+};
+
+MemoryManager::MemoryManager(int mem, int page_size, std::string &filename) :
+  current_time_(0) {
+  std::ifstream ifs;	
+
+  if (!(mem % page_size == 0)) {
+	  std::cout << "ERROR: Memory size must be a multiple of the page!";
+	  exit(1);
+  }
+
+  ifs.open(filename);
+  if (!ifs.is_open()) {
+	std::cout << "ERROR: Could not open file!\n";
+    exit(1);
+  }
+
+	int num_frames = mem / page_size;
+	fl_ = FrameList(num_frames, page_size);
+
+  int num_procs, counter, total_space, life_time, arrival;
+	int pid, tmp, num_space;
+
+	ifs >> num_procs;
+	pl_ = std::vector<Process>(num_procs);
+
+	while (!ifs.eof() && counter < num_procs) {
+		// store temp values for processes
+		ifs >> pid >> arrival >> life_time >> num_space;
+		total_space = 0;
+		for (int i = 0; i < num_space; ++i) {
+			ifs >> tmp;
+			total_space += tmp;
+		}
+		Process proc(pid, arrival, life_time, total_space);
+		pl_[counter++] = proc;
+  }
+	ifs.close();
+}
+
+void MemoryManager::enqueue_arrivals() {
+  for (auto &proc : pl_) {
+	  if (proc.get_arrival() == current_time_) {
+			std::cout << get_prefix() <<
+			  "Process " << proc.get_pid() << " arrives" << std::endl;
+		      
+			pq_.push_back(proc);
+      print_process_queue();
+
+      fl_.print();
+		}
+	}
+}
+
+void MemoryManager::print_process_queue() const {
+	std::cout << "\tInput queue: [";
+
+	for (auto el : pq_) {
+   std::cout << el.get_pid();
+	}
+	std::cout <<  "]\n";
+}
+
+std::string MemoryManager::get_prefix() {
+	std::string result = "\t";
+
+  if (last_announcement_ != current_time_) 
+		result = "t = " + std::to_string(current_time_) + ", ";
+
+	last_announcement_ = current_time_;
+
+	return result;
+}
+
+void MemoryManager::terminate_completed_processes() {
+	// dequeue any procs that need it
+	for (int i = 0; i < pl_.size(); ++i) {
+
+	  if (pl_[i].active()) {
+			int time_elapsed = current_time_ - pl_[i].get_load_time();
+	    if (time_elapsed >= pl_[i].get_life()) {
+			  std::cout << get_prefix()
+          << "process " << pl_[i].get_pid() << " completes\n";
+
+			  pl_[i].end(current_time_);
+			  fl_.free_by_pid(pl_[i].get_pid());
+			  fl_.print();
+		  }
+	  }
+  }
+}
+
+void MemoryManager::update_pl(int pid) {
+  for (auto &el : pl_) 
+		if (el.get_pid() == pid) 
+		  el.load_to_mem(current_time_);
+}
+
+void MemoryManager::assign_available_memory() {
+	for (int i = 0; i < pq_.size(); ++i) {
+		Process proc = pq_[i];
+
+		if (fl_.process_fits(proc)) {
+			std::cout << get_prefix() 
+				<< "MM moves Process " << proc.get_pid() << " to memory\n";
+			
+		  fl_.fit_process(proc);
+      
+			update_pl(proc.get_pid());
+		
+		  pq_.erase(pq_.begin() + i);
+		  print_process_queue();
+			assign_available_memory();
+		  fl_.print();		
+	  }
+	}
+}
+
+void MemoryManager::print_turnaround_time()  {
+	float total = 0;
+	for (auto proc : pl_) 
+		total += proc.get_time_done() - proc.get_arrival();
+	
+  float avg = total / pl_.size();
+	std::cout << "Average Turnaround Time: " << avg << std::endl;
+}
+
+void MemoryManager::simulate() {
+	
+	do { 
+		enqueue_arrivals();
+    terminate_completed_processes();
+		assign_available_memory();
+		++current_time_;
+
+		if (current_time_ > MAX_TIME) {
+			std::cout << "DEADLOCK: max time reached\n";
+			break;
+		}
+
+	} while (!(pq_.empty() && fl_.empty()));
+
+	print_turnaround_time();
+}
